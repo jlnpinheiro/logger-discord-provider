27bd198509aebdbfc3dc72950b3df6c2c46d4edf
Jorge Nogueira <jlnpinheiro@gmail.com>
2020-05-09 02:35:43 -0300

    Criação de um arquivo enviado como anexo, com as informações da exception.

--- logger-discord-provider/DiscordLogger.cs
+++ logger-discord-provider/DiscordLogger.cs
@@ -27,113 +27,128 @@ namespace JNogueira.Logger.Discord
 
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
-            if (formatter == null)
-                throw new ArgumentNullException(nameof(formatter));
-
-            var formattedMessage = formatter(state, exception);
-
-            if (string.IsNullOrEmpty(formattedMessage))
-                return;
-
-            DiscordMessage message = null;
-
-            DiscordMessageEmbed embed = null;
-
-            switch (logLevel)
-            {
-                case LogLevel.None:
-                case LogLevel.Trace:
-                    message = new DiscordMessage(logLevel.ToString(), _options.UserName);
-                    embed = new DiscordMessageEmbed(formattedMessage);
-                    break;
-                case LogLevel.Debug:
-                    message = new DiscordMessage($"{DiscordEmoji.SpiderWeb} {logLevel.ToString()}", _options.UserName);
-                    embed = new DiscordMessageEmbed(formattedMessage);
-                    break;
-                case LogLevel.Information:
-                    message = new DiscordMessage($"{DiscordEmoji.InformationSource} {logLevel.ToString()}", _options.UserName);
-                    embed = new DiscordMessageEmbed(formattedMessage, 31743);
-                    break;
-                case LogLevel.Warning:
-                    message = new DiscordMessage($"{DiscordEmoji.Warning} {logLevel.ToString()}", _options.UserName);
-                    embed = new DiscordMessageEmbed(formattedMessage, 16761095);
-                    break;
-                case LogLevel.Critical:
-                    message = new DiscordMessage($"{DiscordEmoji.Radioactive} {logLevel.ToString()}", _options.UserName);
-                    embed = new DiscordMessageEmbed(formattedMessage, 16711680);
-                    break;
-                case LogLevel.Error:
-                    message = new DiscordMessage($"{DiscordEmoji.Skull} {logLevel.ToString()}", _options.UserName);
-                    embed = new DiscordMessageEmbed(formattedMessage, 14431557);
-                    break;
-            }
-
-            var fields = new List<DiscordMessageEmbedField>();
-
-            if (!string.IsNullOrEmpty(_options.ApplicationName))
-                fields.Add(new DiscordMessageEmbedField("Application name", _options.ApplicationName));
-
-            if (!string.IsNullOrEmpty(_options.EnvironmentName))
-                fields.Add(new DiscordMessageEmbedField("Environment name", _options.EnvironmentName));
-
-            var files = new List<DiscordFile>();
-
-            if (exception != null)
-            {
-                embed.Title = exception.Message;
-                
-                embed.Description = formattedMessage;
-
-                fields.Add(new DiscordMessageEmbedField("Exception type", exception.GetType().ToString()));
-
-                if (!string.IsNullOrEmpty(exception.Source))
-                    fields.Add(new DiscordMessageEmbedField("Source", exception.Source));
-
-                if (exception.GetBaseException()?.Message != exception.Message)
-                    fields.Add(new DiscordMessageEmbedField("Base exception", exception.GetBaseException()?.Message));
-
-                foreach (DictionaryEntry data in exception.Data)
-                    fields.Add(new DiscordMessageEmbedField(data.Key.ToString(), data.Value.ToString()));
-
-                if (_httpContextAcessor != null)
-                {
-                    var uriBuilder = new UriBuilder
-                    {
-                        Scheme = _httpContextAcessor.HttpContext?.Request?.Scheme,
-                        Host = _httpContextAcessor.HttpContext?.Request?.Host.Host,
-                        Path = _httpContextAcessor.HttpContext?.Request?.Path.ToString(),
-                        Query = _httpContextAcessor.HttpContext?.Request?.QueryString.ToString()
-                    };
-
-                    if (_httpContextAcessor.HttpContext?.Request?.Host.Port.HasValue == true && _httpContextAcessor.HttpContext?.Request?.Host.Port.Value != 80)
-                        uriBuilder.Port = _httpContextAcessor.HttpContext.Request.Host.Port.Value;
-
-                    fields.Add(new DiscordMessageEmbedField("URL", uriBuilder.Uri.ToString()));
-
-                    var requestHeaders = new Dictionary<string, string>();
-
-                    foreach (var item in _httpContextAcessor.HttpContext?.Request?.Headers?.Where(x => x.Key != "Cookie" && x.Value.Count > 0))
-                        requestHeaders.Add(item.Key, string.Join(",", item.Value.ToArray()));
-
-                    if (requestHeaders.Count > 0)
-                        files.Add(new DiscordFile("request-header.txt", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(requestHeaders, Formatting.Indented))));
-                }
-
-                files.Add(new DiscordFile("stack-trace.txt", Encoding.UTF8.GetBytes(exception.StackTrace)));
-            }
-
-            embed.Fields = fields.ToArray();
-
-            message.Embeds = new[] { embed };
-
             var client = new DiscordWebhookClient(_options.WebhookUrl);
 
-            if (files.Count > 0)
+            try
             {
-                client.SendToDiscord(message, files.ToArray()).Wait();
+                if (formatter == null)
+                    throw new ArgumentNullException(nameof(formatter));
+
+                var formattedMessage = formatter(state, exception);
+
+                if (string.IsNullOrEmpty(formattedMessage))
+                    return;
+
+                DiscordMessage message = null;
+
+                DiscordMessageEmbed embed = null;
+
+                switch (logLevel)
+                {
+                    case LogLevel.None:
+                    case LogLevel.Trace:
+                        message = new DiscordMessage(logLevel.ToString(), _options.UserName);
+                        embed = new DiscordMessageEmbed(formattedMessage);
+                        break;
+                    case LogLevel.Debug:
+                        message = new DiscordMessage($"{DiscordEmoji.SpiderWeb} {logLevel.ToString()}", _options.UserName);
+                        embed = new DiscordMessageEmbed(formattedMessage);
+                        break;
+                    case LogLevel.Information:
+                        message = new DiscordMessage($"{DiscordEmoji.InformationSource} {logLevel.ToString()}", _options.UserName);
+                        embed = new DiscordMessageEmbed(formattedMessage, 31743);
+                        break;
+                    case LogLevel.Warning:
+                        message = new DiscordMessage($"{DiscordEmoji.Warning} {logLevel.ToString()}", _options.UserName);
+                        embed = new DiscordMessageEmbed(formattedMessage, 16761095);
+                        break;
+                    case LogLevel.Critical:
+                        message = new DiscordMessage($"{DiscordEmoji.Radioactive} {logLevel.ToString()}", _options.UserName);
+                        embed = new DiscordMessageEmbed(formattedMessage, 16711680);
+                        break;
+                    case LogLevel.Error:
+                        message = new DiscordMessage($"{DiscordEmoji.Skull} {logLevel.ToString()}", _options.UserName);
+                        embed = new DiscordMessageEmbed(formattedMessage, 14431557);
+                        break;
+                }
+
+                var fields = new List<DiscordMessageEmbedField>();
+
+                if (!string.IsNullOrEmpty(_options.ApplicationName))
+                    fields.Add(new DiscordMessageEmbedField("Application name", _options.ApplicationName));
+
+                if (!string.IsNullOrEmpty(_options.EnvironmentName))
+                    fields.Add(new DiscordMessageEmbedField("Environment name", _options.EnvironmentName));
+
+                var files = new List<DiscordFile>();
+
+                if (exception != null)
+                {
+                    embed.Title = exception.Message;
+
+                    embed.Description = formattedMessage;
+
+                    var exceptionInfoText = new StringBuilder();
+
+                    exceptionInfoText.AppendFormat("Message: {0}\r\n", exception.Message);
+
+                    exceptionInfoText.AppendFormat("Exception type: {0}\r\n", exception.GetType().ToString());
+
+                    exceptionInfoText.AppendFormat("Source: {0}\r\n", exception.Source);
+
+                    exceptionInfoText.AppendFormat("Base exception: {0}\r\n", exception.GetBaseException()?.Message);
+
+                    foreach (DictionaryEntry data in exception.Data)
+                        exceptionInfoText.Append($"{data.Key}: {data.Value}\r\n");
+
+                    exceptionInfoText.AppendFormat("Stack trace: {0}\r\n", exception.StackTrace);
+
+                    if (_httpContextAcessor != null)
+                    {
+                        var uriBuilder = new UriBuilder
+                        {
+                            Scheme = _httpContextAcessor.HttpContext?.Request?.Scheme,
+                            Host = _httpContextAcessor.HttpContext?.Request?.Host.Host,
+                            Path = _httpContextAcessor.HttpContext?.Request?.Path.ToString(),
+                            Query = _httpContextAcessor.HttpContext?.Request?.QueryString.ToString()
+                        };
+
+                        if (_httpContextAcessor.HttpContext?.Request?.Host.Port.HasValue == true && _httpContextAcessor.HttpContext?.Request?.Host.Port.Value != 80)
+                            uriBuilder.Port = _httpContextAcessor.HttpContext.Request.Host.Port.Value;
+
+                        fields.Add(new DiscordMessageEmbedField("URL", uriBuilder.Uri.ToString()));
+
+                        var requestHeaders = new Dictionary<string, string>();
+
+                        foreach (var item in _httpContextAcessor.HttpContext?.Request?.Headers?.Where(x => x.Key != "Cookie" && x.Value.Count > 0))
+                            requestHeaders.Add(item.Key, string.Join(",", item.Value.ToArray()));
+
+                        if (requestHeaders.Count > 0)
+                            exceptionInfoText.AppendFormat("Request headers: {0}\r\n", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(requestHeaders, Formatting.Indented)));
+                    }
+
+                    files.Add(new DiscordFile("exception-info.txt", Encoding.UTF8.GetBytes(exceptionInfoText.ToString())));
+                }
+
+                embed.Fields = fields.ToArray();
+
+                message.Embeds = new[] { embed };
+
+                if (files.Count > 0)
+                {
+                    client.SendToDiscord(message, files.ToArray()).Wait();
+                }
+                else
+                {
+                    client.SendToDiscord(message).Wait();
+                }
             }
-            else
+            catch (Exception ex)
             {
+                var message = new DiscordMessage($"{DiscordEmoji.Skull} {logLevel.ToString()}", _options.UserName);
+
+                message.Embeds = new[] { new DiscordMessageEmbed(ex.Message, 14431557, description: "Error logging message to Discord") };
+
                 client.SendToDiscord(message).Wait();
             }
         }
